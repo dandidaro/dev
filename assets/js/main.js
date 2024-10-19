a92961a5fad375a5f7591cea2a96426c60291084
Dandi Darojat <66661207+dandidaro@users.noreply.github.com>
2024-10-19 16:38:46 +0700

    v0.3
    - adding bootstrap for the sake of layouting
    - removing locomotive, change to scrollspy
    - update on js files (too much, cant remember what im updating)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,24 @@
-/* Locomotive scroll */
-const scroller = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-  scrollFromAnywhere: true,
-  reloadOnContextChange: true,
-  multiplier: 0.5
+/* Remove hash on url when click on navbar */
+$('.page-scroll').click(function(e){
+  setTimeout(function() {
+    history.replaceState(null, null, ' ');
+  }, 0);
 });
-
-// Set all variables
-const divhero = document.querySelector('[data-scroll-id="hero-id"');
-const navbarHome = document.getElementById("nav-home");
-const navbarWorks = document.getElementById("nav-works");
-const navbarAbout = document.getElementById("nav-about");
-
-const targetHome = document.getElementById("hero-id");
-const targetWorks = document.getElementById("works-id");
-const targetAbout = document.getElementById("about-id");
-
-
-// All on-scroll animation purposes
-scroller.on('scroll', (args) => {
-  if(typeof args.currentElements['hero-id'] === 'object') {
-    let progress = args.currentElements['hero-id'].progress;
-    divhero.style.opacity = 4 - ((progress * 7) - 1);
-  }
-});
-
-scroller.on('call', (navMenu, state, event) => {
-  if(state === "enter"){
-    if(navMenu === "nav_home"){
-      navbarHome.classList.add('active');
-      navbarWorks.classList.remove('active');
-      // navbarAbout.classList.remove('active');
-      console.log(navMenu, state);
-    } else {
-      navbarHome.classList.remove('active');
-    }
-    if(navMenu === "nav_works"){
-      navbarWorks.classList.add('active');
-      navbarHome.classList.remove('active');
-      navbarAbout.classList.remove('active');
-      console.log(navMenu, state);
-    } else {
-      navbarWorks.classList.remove('active');
-    }
-    if(navMenu === "nav_about"){
-      navbarAbout.classList.add('active');
-      navbarWorks.classList.remove('active');
-      // navbarHome.classList.remove('active');
-      console.log(navMenu, state);
-    } else {
-      navbarAbout.classList.remove('active');
-    }
-  }
-});
-
-// Navbar on-click purposes
-navbarHome.addEventListener("click", function(){
-  scroller.scrollTo(0);
-});
-
-navbarWorks.addEventListener("click", function(){
-  scroller.scrollTo(targetWorks, {'offset' : -100});
-});
-
-navbarAbout.addEventListener("click", function(){
-  scroller.scrollTo(targetAbout, {'offset' : -100});
-});
-
 
 /* Perspective on hero */
 let constrain = 100;
-let mouseOverContainer = document.getElementById("hero-id");
-let ex1Layer = document.getElementById("tilted");
+let mouseOverHome = document.querySelector('#home');
+let mouseOverWorks = document.querySelector('#works');
+let ex1Layer = document.querySelector("#tilted");
+let ex2Layer = document.querySelector("#tilted2");
 
 function transforms(x, y, el) {
   let box = el.getBoundingClientRect();
   let calcX = -(y - box.y - (box.height / 2)) / constrain;
   let calcY = (x - box.x - (box.width / 2)) / constrain;
+  let elHeight = box.height;
   
-  return "perspective(250px) "
+  return "perspective(" + elHeight + "px) "
     + "   rotateX("+ calcX +"deg) "
     + "   rotateY("+ calcY +"deg) ";
 };
@@ -88,11 +27,20 @@ function transforms(x, y, el) {
   el.style.transform  = transforms.apply(null, xyEl);
 }
 
-mouseOverContainer.onmousemove = function(e) {
+mouseOverHome.onmousemove = function(e) {
   let xy = [e.clientX, e.clientY];
   let position = xy.concat([ex1Layer]);
 
   window.requestAnimationFrame(function(){
     transformElement(ex1Layer, position);
+  });
+};
+
+mouseOverWorks.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex2Layer, position);
   });
 };
